a87e3c075776106f5dc3ebb0830f6b8c19ece3c7
Igor Vahrameev <igorvahrameev@gmail.com>
2019-05-02 08:21:44 -0600

    Basic Email service structure

--- EmailService/emailService.py
+++ EmailService/emailService.py
@@ -1,1 +1,23 @@
-# The email service will go here
+import smtplib, pyzmail, re
+from imapclient import IMAPClient
+imaplib._MAXLINE = 10000000 # Size limit for search
+
+
+
+# Email Service class will take emails, send them, check emails for instructions.
+class EmailService():
+	def __init__(self):
+		# Set up the Imap client, and log in.
+		self.imapObj= IMAPClient('imap.gmail.com', ssl=True) 
+		print(self.imapObj.login('cs3030pytester@gmail.com', sys.argv[1])) # Change to read from encrypted file
+		self.imapObj.select_folder('INBOX')
+
+	# Send Email will validate email, compose message, send. Return True if sent, False if not
+	def sendEmail(self, email, message):
+		# Check if email is valid
+		emailRegex = re.compile('\S+@\S+')
+		if emailRegex.findall(email) == None:
+			print("Error: Invalid Email.")
+			return False
+
+		# Set up email 
