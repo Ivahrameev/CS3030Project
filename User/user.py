1282207bcfce195278fa5608da94a832a0ed91e2
Igor Vahrameev <igorvahrameev@gmail.com>
2019-05-02 09:03:55 -0600

    More Structure

--- User/user.py
+++ User/user.py
@@ -1,8 +1,9 @@
 # This is where the user object will go
 
 class User():
-	def __init__(self, username, email, notificationTime):
+	def __init__(self, username, email, notificationTime, zipCode):
 		self.username = username
 		self.email = email
 		self.notificationTime = notificationTime
+		self.zipCode = zipCode
 		
