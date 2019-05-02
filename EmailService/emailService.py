6a2ac85229766e908de1a22171349b9df9c93f19
Igor Vahrameev <igorvahrameev@gmail.com>
2019-05-02 08:45:44 -0600

    Working email Service

--- EmailService/emailService.py
+++ EmailService/emailService.py
@@ -1,4 +1,4 @@
-import smtplib, pyzmail, re
+import smtplib, pyzmail, re, sys, datetime, imaplib
 from imapclient import IMAPClient
 imaplib._MAXLINE = 10000000 # Size limit for search
 
@@ -7,10 +7,17 @@ imaplib._MAXLINE = 10000000 # Size limit for search
 # Email Service class will take emails, send them, check emails for instructions.
 class EmailService():
 	def __init__(self):
-		# Set up the Imap client, and log in.
+		# Set up the Imap client, and log in. Will be used for email searching
+		self.email = 'cs3030pytester@gmail.com'
 		self.imapObj= IMAPClient('imap.gmail.com', ssl=True) 
-		print(self.imapObj.login('cs3030pytester@gmail.com', sys.argv[1])) # Change to read from encrypted file
+		print(self.imapObj.login(self.email, sys.argv[1])) # Change to read from encrypted file
 		self.imapObj.select_folder('INBOX')
+
+		# Set up SMTP stuff, this is what will send the email.
+		self.smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+		print(self.smtpObj.starttls())
+		print(self.smtpObj.login(self.email, sys.argv[1]))
+
 
 	# Send Email will validate email, compose message, send. Return True if sent, False if not
 	def sendEmail(self, email, message):
@@ -21,3 +28,11 @@ class EmailService():
 			return False
 
 		# Set up email 
+		date = datetime.datetime.now().strftime('%m-%d-%y')
+		composedMessage = f'Subject: Your lunch deals {date} \n\n Checkout these deals: {message}'
+		self.smtpObj.sendmail(self.email, email, composedMessage)
+		print("Email sent to " + email)
+
+	def end(self):
+		self.smtpObj.quit()
+		self.imapObj.logout()
--- lunchTimeController.py
+++ lunchTimeController.py
@@ -1,1 +1,10 @@
 # This will be the app's main controller.
+import os
+from EmailService.emailService import EmailService
+
+def test():
+	myObj = EmailService()
+	myObj.sendEmail('cs3030pytester@gmail.com', 'Hello world')
+	myObj.end()
+
+test()
