3799cf00f94a3c6e32fba69f177604facbce0023
Dillon <ddivich@uccs.edu>
2019-05-12 23:24:44 -0600

    Update user.py
    added a function to write users to a json file

--- User/user.py
+++ User/user.py
@@ -8,3 +8,14 @@ class User():
 		self.zipCode = zipCode
 		self.preference = preference
 		
+		
+       def writeUserToJson(self):
+        data['Users'].append({
+         'username' : self.username,
+         'email' : self.email,
+         'notification time' : self.notificationTime,
+         'zipcode' : self.zipCode,
+         'preference' : self.preference
+        })  
+        with open ('userList.txt', 'w') as outfile:
+        json.dump(data, outfile, sort_keys=True, indent=4)
