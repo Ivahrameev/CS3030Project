4e30aca2d34ac0746c676d14eda41a23af01ad35
Dillon <ddivich@uccs.edu>
2019-05-14 23:10:14 -0600

    made ID generation more robust

--- User/user.py
+++ User/user.py
@@ -8,14 +8,44 @@ data['Users'] = ['UserId']
 
 class User():
 	
+	
 	def __init__(self, username, email, notificationTime, zipCode, preference):
          self.username = username
 	 self.email = email
 	 self.notificationTime = notificationTime
 	 self.zipCode = zipCode
 	 self.preference = preference
-	 self.uniqueId = random.randint(1,9999999999999)
+	 self.uniqueId = self.generateUniqueId()
+	   
+	
+        def generateUniqueId(self):
+         random.seed()
+         numberList = [] 
+         listChoice = []
+         number = []
+         divisorList = [1,10,100,1000,10000]
+         #generates 1000 random numbers
+         for x in range(1000): 
+          numberList.append(random.randint(1,9999999999)) 
+         #randomly picks 100 of the 1000 numbers
+         for x in range(100): 
+          listChoice.append(random.choice(numberList))
+         #generates an number from four other numbers via a lambda function
+         randomNumber = lambda NumA, NumB, DivisorA, DivisorB : NumA/DivisorA + NumB/DivisorB 
+         #generates ten random numbers by picking two numbers from a list of 
+         #randomly generated numbers and randomly picks two divisors from a list
+         for x in range(10):
+          number.append(randomNumber(random.choice(listChoice),
+                                     random.choice(listChoice),
+                                     random.choice(divisorList),
+                                     random.choice(divisorList)))
+         #returns a very random integer that is generated from four other numbers
+         return int(randomNumber(random.choice(number), 
+                                 random.choice(number), 
+                                 random.choice(divisorList),
+                                 random.choice(divisorList)))
 		
+	
        #this function writes a given user to a json file
        #does not write user with their ID, used to create original 'userList.txt' file
        def writeUserToJson(self):
@@ -28,7 +58,8 @@ class User():
         })  
         with open ('userList.txt', 'w') as outfile:
         json.dump(data, outfile, sort_keys=True, indent=4)
-           
+          
+		
        #writes the given user to a json file with a unique user id
        #id used for sorting and to prevent old data from being overwritten
        def writeUserIdToJson(self):
@@ -42,6 +73,7 @@ class User():
         with open ('UserList.txt', 'w') as outfile:
          json.dump(data, outfile, sort_keys=True, indent=4)   
 	
+	
        #returns a list with all the users
        #currently only works with 'userList.txt'
        def getUserFromJson(self):
@@ -52,6 +84,8 @@ class User():
           pulledUser.append({'username':person['username'], 'email':person['email'], 'notification time':person['notification time'], 'zipcode':person['zipcode'], 'preference':person['preference'] })
          return pulledUser
 
+	
+	
 #returns a dictionary of the user base
 #will work with both the old and new 'UserList.txt' file
 def getUserIdFromJson():
