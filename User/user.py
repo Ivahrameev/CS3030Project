e75cadbf0db64aeaa265e7f56fa361c9cd31f86b
Dillon <ddivich@uccs.edu>
2019-05-14 19:29:24 -0600

    added writeUserIdToJson(self) method
    the writeUserIdToJson(self) method writes to json file with each user having a 'unique' user id that serves as a key for sorting as well as to prevent old data from being overwritten when called from getUserFromJson(self)
    
    added some comments

--- User/user.py
+++ User/user.py
@@ -1,9 +1,10 @@
 # This is where the user object will go
 
 import json
+import random
 
 data = {}
-data['Users'] = []
+data['Users'] = ['UserId']
 
 class User():
 	
@@ -13,8 +14,10 @@ class User():
 	 self.notificationTime = notificationTime
 	 self.zipCode = zipCode
 	 self.preference = preference
+	 self.uniqueId = random.randint(1,9999999999999)
 		
        #this function writes a given user to a json file
+       #does not write user with their ID, used to create original 'userList.txt' file
        def writeUserToJson(self):
         data['Users'].append({
          'username' : self.username,
@@ -25,8 +28,22 @@ class User():
         })  
         with open ('userList.txt', 'w') as outfile:
         json.dump(data, outfile, sort_keys=True, indent=4)
+           
+       #writes the given user to a json file with a unique user id
+       #id used for sorting and to prevent old data from being overwritten
+       def writeUserIdToJson(self):
+        data['Users'].append({self.uniqueId : {
+         'username' : self.username, 
+         'email' : self.email, 
+         'notification time' : self.notificationTime, 
+         'zipcode' : self.zipCode, 
+         'preference' : self.preference}
+        })
+        with open ('UserList.txt', 'w') as outfile:
+         json.dump(data, outfile, sort_keys=True, indent=4)   
 	
        #returns a list with all the users
+       #currently only works with 'userList.txt'
        def getUserFromJson(self):
         pulledUser = []
         with open('userList.txt') as json_file:
